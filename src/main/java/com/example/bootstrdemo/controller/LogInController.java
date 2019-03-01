423348428abae821ce3e098cba097df6501b31af
zhaowei.zhang01 <zhaowei.zhang01@hand-china.com>
2019-03-01 13:46:30 +0800

    strdemo测试项目

---
+++ src/main/java/com/example/bootstrdemo/controller/LogInController.java
@@ -0,0 +1,29 @@
+package com.example.bootstrdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @author: x.x@hand-china.com
+ * @date: 2019/3/1
+ */
+@Controller
+public class LogInController {
+
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index(){
+        return "index.html";
+    }
+
+
+    @PostMapping("/test")
+    public String test2(){
+        return "aaaaaa";
+    }
+}
